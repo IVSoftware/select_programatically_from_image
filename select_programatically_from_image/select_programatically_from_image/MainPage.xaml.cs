36451b131befe4d2994bcc9ab55f09a5702c0ac2
Tom Gregor <tgregor@ivsoftware.com>
2022-06-30 10:15:44 -0600

    Add project files.

---
+++ select_programatically_from_image/select_programatically_from_image/MainPage.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace select_programatically_from_image
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            BindingContext = new MainFrameBinding();
+            InitializeComponent();
+        }
+
+        private void delete_Clicked(object sender, EventArgs e)
+        {
+            if(sender is ImageButton imageButton)
+            {
+                Flist.SelectedItem = imageButton.BindingContext;
+            }
+        }
+
+        private async void Flist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var fileName = ((DataModel)e.SelectedItem).fname;
+            if(await App.Current.MainPage.DisplayAlert(
+                "delete file", 
+                fileName, accept: "OK", 
+                cancel: "CANCEL"))
+            {
+                System.Diagnostics.Debug.WriteLine($"DELETED {fileName}");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Cancelled!");
+            }
+        }
+    }
+
+    class MainFrameBinding : INotifyPropertyChanged
+    {
+        public MainFrameBinding()
+        {
+            Items.Add(new DataModel { fname = "rtegdgg.txt" });
+            Items.Add(new DataModel { fname = "mmrtegdgg.txt" });
+        }
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(
+           ref T backingStore,
+           T value,
+           [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            {
+                return false;
+            }
+            backingStore = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion INotifyPropertyChanged
+        public ObservableCollection<DataModel> Items { get; set; } = new ObservableCollection<DataModel>();
+    }
+    class DataModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(
+           ref T backingStore,
+           T value,
+           [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            {
+                return false;
+            }
+            backingStore = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        #endregion INotifyPropertyChanged
+
+        string _fname = string.Empty;
+        public string fname
+        {
+            get => _fname;
+            set => SetProperty(ref _fname, value);
+        }
+    }
+}
