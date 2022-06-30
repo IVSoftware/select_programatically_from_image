1ab1af857bb2484eee9055eeefa2dfc1482c57c8
Tom Gregor <tgregor@ivsoftware.com>
2022-06-30 12:23:08 -0600

    MVVM version with functional delete-cancel

--- select_programatically_from_image/select_programatically_from_image/MainPage.xaml.cs
+++ select_programatically_from_image/select_programatically_from_image/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace select_programatically_from_image
@@ -16,37 +17,23 @@ namespace select_programatically_from_image
         {
             BindingContext = new MainFrameBinding();
             InitializeComponent();
+            _debug = Flist;
         }
-
-        private async void delete_Clicked(object sender, EventArgs e)
-        {
-            if (sender is ImageButton imageButton)
-            {
-                Flist.SelectedItem = imageButton.BindingContext;
-                var fileName = ((DataModel)Flist.SelectedItem).fname;
-                if (await App.Current.MainPage.DisplayAlert(
-                    "delete file",
-                    fileName, accept: "OK",
-                    cancel: "CANCEL"))
-                {
-                    System.Diagnostics.Debug.WriteLine($"DELETED {fileName}");
-                }
-                else
-                {
-                    System.Diagnostics.Debug.WriteLine("Cancelled!");
-                }
-            }
-        }
-
-        private async void Flist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-        {
-        }
+        public static ListView _debug = null;
     }
 
     class MainFrameBinding : INotifyPropertyChanged
     {
         public MainFrameBinding()
         {
+            DataModel.Delete += (sender, e) =>
+            {
+                Items.Remove((DataModel)sender);
+            };
+            DataModel.Select += (sender, e) =>
+            {
+                SelectedItem = (DataModel)sender;
+            };
             Items.Add(new DataModel { fname = "rtegdgg.txt" });
             Items.Add(new DataModel { fname = "mmrtegdgg.txt" });
         }
@@ -71,9 +58,20 @@ namespace select_programatically_from_image
         }
         #endregion INotifyPropertyChanged
         public ObservableCollection<DataModel> Items { get; set; } = new ObservableCollection<DataModel>();
+        DataModel _SelectedItem = null;
+        public DataModel SelectedItem
+        {
+            get => _SelectedItem;
+            set => SetProperty(ref _SelectedItem, value);
+        }
+
     }
     class DataModel : INotifyPropertyChanged
     {
+        public DataModel()
+        {
+            DeleteFileWithPromptCommand = new Command<DataModel>(OnDeleteFileWithPromptCommand);
+        }
         #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -101,5 +99,25 @@ namespace select_programatically_from_image
             get => _fname;
             set => SetProperty(ref _fname, value);
         }
+
+        public ICommand DeleteFileWithPromptCommand { get; private set; }
+        private async void OnDeleteFileWithPromptCommand(DataModel item)
+        {
+            Select?.Invoke(this, EventArgs.Empty);
+            var fileName = item.fname;
+            if (await App.Current.MainPage.DisplayAlert(
+                "delete file",
+                fileName, accept: "OK",
+                cancel: "CANCEL"))
+            {
+                Delete?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Cancelled!");
+            }
+        }
+        public static event EventHandler Select;
+        public static event EventHandler Delete;
     }
 }
